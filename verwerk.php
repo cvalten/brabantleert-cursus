f3473e58bc949948db507a77e7a0c9c49d1a0879
cvalten <info@ceesvanalten.nl>
2023-10-30 13:11:02 +0100

    Lesstof 23-10 aangepast en verwerk form veranderd

--- verwerk.php
+++ verwerk.php
@@ -1,11 +1,9 @@
 <?php
 //Definieer de variabelen als leeg.
 $voornaam = $achternaam =$email=$bericht= "";
-print_R($_GET);
 //Als er iets gepost is (de knop is ingedrukt)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //Vul de variabelen met de juiste inhoud.
-
 //TODO: Controleer of de waardes gevuld zijn en niet leeg.
     $voornaam = ($_POST['voornaam']);
     $achternaam = ($_POST['achternaam']);
@@ -13,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $datum = $_POST['datum'];
     $bericht = ($_POST['bericht']);
 }
-//Anders zijn het boefjes
+//Anders proberen ze iets wat niet mag
 else{
     //En stuur je ze terug naar het contact formulier.
     header("Location: contact.php");
@@ -27,7 +25,6 @@ else{
 ?>
     <main style="border: 6px double rgb(136, 210, 136);">
 <?php
-
 print_r($_POST);
 //TODO: Verwerk inhoud naar een excel bestand 
 //Todo: Mail klant.
