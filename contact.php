c99b5096ea921b496f03cd383b0eb3254da7a394
cvalten <info@ceesvanalten.nl>
2023-10-30 16:15:24 +0100

    Validatie op pagina zelf

--- contact.php
+++ contact.php
@@ -4,6 +4,50 @@
     $header ="Vul onderstaand formulier in :)";
     include("includes/head.inc.php");
     include("includes/nav.inc.php");
+   // BEGIN Validatie formulier
+//Definieer de variabelen als leeg.
+$voornaam = $achternaam =$email=$bericht=$error= "";
+$showform = true;
+$errorCount = 0;
+
+//Als er iets gepost is (de knop is ingedrukt)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//Vul de variabelen met de juiste inhoud.
+//TODO: Controleer of de waardes gevuld zijn en niet leeg.
+    $voornaam = ($_POST['voornaam']);
+    $achternaam = ($_POST['achternaam']);
+    $email = ($_POST['email']);
+//    $datum = $_POST['datum'];
+    $bericht = ($_POST['bericht']);
+
+    //Controleer of een variabele nog leeg is.
+
+    if(empty($voornaam)){
+        //Maak een error variabele aan voor de voornaam
+        $errorVoornaam = "Voornaam is leeg!";
+        $errorCount++;
+    }
+    if(empty($achternaam)){
+        //Gebruik de standaard error variabele.
+        $error .= "Achternaam is leeg! <br/>";
+        $errorCount++;
+    }
+    if(empty($email)){
+        $error .= "Email is  niet leeg!<br/>";
+        $errorCount++;
+    }
+    //Valideer of ingevulde data gelijk is aan wat ik als developer wil dat het is.
+    if($voornaam == "Cees"){
+        $error .= "Naam is Cees!<br/>";
+        $errorCount++;
+    }
+
+    if($errorCount == 0){
+        $showform = false;
+    }
+}
+
+
 ?>
     <main style="border: 6px double rgb(136, 210, 136);">
 <a href="verwerk.php?lang=nl">NL</a>
